296815884568d2e63a6eb74f97a569954bd885db
hwoodwri <hwoodwri@gmail.com>
2021-09-12 16:15:36 +0300

    ex06 day01

---
+++ Module-01/ex06(Karen-filter)/Karen.cpp
@@ -0,0 +1,43 @@
+#include "Karen.hpp"
+
+Karen::Karen() {}
+
+void Karen::debug() {
+	std::cout << "[ DEBUG ]" << std::endl;
+	std::cout << "I love to get extra bacon for my 7XL-double-cheese-triple-"
+				 "pickle-special-ketchup burger. I just love it!" << std::endl;
+}
+
+void Karen::info() {
+	std::cout << "[ INFO ]" << std::endl;
+	std::cout << "I cannot believe adding extra bacon cost more money. You "
+				 "don’t put enough! If you did I would not have to ask for it!"
+				 << std::endl;
+}
+
+void Karen::warning() {
+	std::cout << "[ WARNING ]" << std::endl;
+	std::cout <<  "I think I deserve to have some extra bacon for free. I’ve "
+				  "been coming here for years and you just started working "
+				  "here last month." << std::endl;
+}
+
+void Karen::error() {
+	std::cout << "[ ERROR ]" << std::endl;
+	std::cout << "This is unacceptable, I want to speak to the manager now."
+	<< std::endl;
+}
+
+void Karen::complain(std::string level) {
+	typedef void (Karen::*fncPtr)();
+	fncPtr karenFncs[] = {&Karen::debug, &Karen::info, &Karen::warning, &Karen::error};
+	std::string logLevels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	for (int i(0); i < 4; i++) {
+		if (logLevels[i] == level) {
+			(this->*(karenFncs[i]))();
+			return;
+		}
+	}
+}
+
+Karen::~Karen() {}
--- Module-01/ex06(Karen-filter)/main.cpp
+++ Module-01/ex06(Karen-filter)/main.cpp
@@ -1,6 +1,50 @@
-#include <iostream>
+#include "Karen.hpp"
 
-int main() {
-	std::cout << "Hello, World!" << std::endl;
+int main(int argc, char *argv[]) {
+
+	if (argc == 2) {
+		std::string logLevels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+		std::string level(argv[1]);
+		int i = 0;
+		while (i < 4) {
+			if (level == logLevels[i]) {
+				break;
+			}
+			i++;
+		}
+		Karen karen;
+		switch (i) {
+			case 0:
+				karen.complain("DEBUG");
+				std::cout << std::endl;
+				karen.complain("INFO");
+				std::cout << std::endl;
+				karen.complain("WARNING");
+				std::cout << std::endl;
+				karen.complain("ERROR");
+				break;
+			case 1:
+				karen.complain("INFO");
+				std::cout << std::endl;
+				karen.complain("WARNING");
+				std::cout << std::endl;
+				karen.complain("ERROR");
+				break;
+			case 2:
+				karen.complain("WARNING");
+				std::cout << std::endl;
+				karen.complain("ERROR");
+				break;
+			case 3:
+				karen.complain("ERROR");
+				break;
+			default:
+				std::cout << "[ Probably complaining about insignificant problems ]"
+				<<std::endl;
+				break;
+		}
+	} else {
+		std::cout << "Wrong number of arguments" << std::endl;
+	}
 	return 0;
 }
