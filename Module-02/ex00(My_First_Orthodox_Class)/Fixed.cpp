989bd6fe7c75c37ca8bb62fa8c1680bf680ac475
hwoodwri <hwoodwri@gmail.com>
2021-09-18 17:49:02 +0300

    ex00 day02

--- Module-02/ex00(My_First_Orthodox_Class)/Fixed.cpp
+++ Module-02/ex00(My_First_Orthodox_Class)/Fixed.cpp
@@ -2,9 +2,19 @@
 
 Fixed::Fixed() {
 	std::cout << "Default constructor called" << std::endl;
-	this->value = 0;
+	this->fixedPoint = 0;
+}
+
+Fixed::Fixed(const Fixed &fixed) {
+	std::cout << "Copy constructor called" << std::endl;
 }
 
 Fixed::~Fixed() {
 	std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits() const {}
+
+void Fixed::setRawBits(const int raw) {
+
 }
--- Module-02/ex00(My_First_Orthodox_Class)/Fixed.hpp
+++ Module-02/ex00(My_First_Orthodox_Class)/Fixed.hpp
@@ -5,14 +5,22 @@
 
 class Fixed {
 private:
-	int value;
-	static const int frBits = 8;
+	int fixedPoint;
+	static const int fractionalBits = 8;
 public:
+	//default constructor
 	Fixed();
-//	Константный метод — это метод, который гарантирует, что не будет изменять
-//	объект или вызывать неконстантные методы класса (поскольку они могут изменить объект).
+
+	//copy constructor
+	Fixed(const Fixed &fixed);
+
+
+	//Константный метод — это метод, который гарантирует, что не будет изменять
+	//объект или вызывать неконстантные методы класса (поскольку они могут изменить объект).
 	int getRawBits(void) const;
 	void setRawBits(int const raw);
+
+	//destructor
 	virtual ~Fixed();
 
 };
