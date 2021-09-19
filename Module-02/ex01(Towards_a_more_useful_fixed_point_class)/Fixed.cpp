98e5fbc503edda946f5623a6c2b3168b0e7fef4e
hwoodwri <hwoodwri@gmail.com>
2021-09-19 18:39:41 +0300

    ex00 ex01 day02

--- Module-02/ex01(Towards_a_more_useful_fixed_point_class)/Fixed.cpp
+++ Module-02/ex01(Towards_a_more_useful_fixed_point_class)/Fixed.cpp
@@ -5,6 +5,16 @@ Fixed::Fixed() {
 	this->fixedPoint = 0;
 }
 
+Fixed::Fixed(const int num) {
+	std::cout << "Int constructor called" << std::endl;
+	fixedPoint = num << fractionalBits;
+}
+
+Fixed::Fixed(const float flNum) {
+	std::cout << "Float constructor called" << std::endl;
+	fixedPoint = roundf(flNum * (1 << fractionalBits));
+}
+
 Fixed::Fixed(const Fixed &fixed) {
 	std::cout << "Copy constructor called" << std::endl;
 	*this = fixed;
@@ -26,6 +36,15 @@ void Fixed::setRawBits(const int raw) {
 	this->fixedPoint = raw;
 }
 
+float Fixed::toFloat() const { return (float)()}
+
+int Fixed::toInt() const { return fixedPoint >> fractionalBits}
+
 Fixed::~Fixed() {
 	std::cout << "Destructor called" << std::endl;
 }
+
+//std::ostream& operator<< (std::ostream out, const Fixed &fixed) {
+//	out << fixed. ;
+//	return out;
+//}
--- Module-02/ex01(Towards_a_more_useful_fixed_point_class)/Fixed.hpp
+++ Module-02/ex01(Towards_a_more_useful_fixed_point_class)/Fixed.hpp
@@ -2,18 +2,25 @@
 #define FIXED_HPP
 
 #include <iostream>
+#include <cmath>
 
 class Fixed {
 private:
 	int fixedPoint;
-	static const int fractopnalBits = 8;
+	static const int fractionalBits = 8;
 public:
 	Fixed();
+	Fixed(const int num);
+	Fixed(const float flNum);
 	Fixed(const Fixed &fixed);
 	Fixed& operator= (const Fixed &fixed);
 	int getRawBits(void) const;
 	void setRawBits(int const raw);
+	float toFloat(void) const;
+	int toInt(void) const;
 	virtual ~Fixed();
 };
+
+std::ostream& operator<< (std::ostream out, const Fixed &fixed);
 
 #endif //FIXED_HPP
